191fda2f9f230e868de7c9ad1fc32a79b47ef720
Luciano Bello <luciano.bello@ibm.com>
2020-12-11 02:59:17 -0500

    nlopt dependency missing

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     packages=['qcommunity'],
     install_requires=[
         'qiskit', 'qiskit_aqua', 'networkx', 'numpy', 'matplotlib', 'joblib',
-        'progressbar2', 'SALib'
+        'progressbar2', 'SALib', 'nlopt'
     ],
     zip_safe=False)
