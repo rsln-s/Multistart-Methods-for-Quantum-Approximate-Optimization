1e8cd15be197ddf5c3e650b5e23e9bb113bfada9
Luciano Bello <luciano.bello@ibm.com>
2020-12-11 13:13:10 +0100

    install from repos automatically (PEP-508)

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ setup(
     packages=['qcommunity'],
     install_requires=[
         'qiskit', 'qiskit_aqua', 'networkx', 'numpy', 'matplotlib', 'joblib',
-        'progressbar2', 'SALib'
+        'progressbar2', 'SALib',
+        'libensemble @ git+ssh://git@github.com/rsln-s/libensemble_var#egg=libensemble&sha1=0a0d1b2bbf10ef1d8cb596e7e401f9e78db01332',
+        'ibmqxbackend @ git+ssh://git@github.com/rsln-s/ibmqxbackend@v1.0-multistart#egg=ibmqxbackend',
     ],
     zip_safe=False)
